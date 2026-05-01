9449e52ea98feb4e3f234f9b0f36f05a7bfee43d
J4CKINS <electronicjack4@gmail.com>
2026-05-01 12:14:16 +0100

    Changed page layout

--- book_library/index.php
+++ book_library/index.php
@@ -6,47 +6,38 @@
 	<body>
 		<center>
 			<h1>Welcome to Online Book Library</h1>
-			<table border="1">
-				<tr>
-					<th>Book Cover</th>
-					<th>Book Info</th>
-				</tr>
- <?php
-  $host='group55-rds-db.chynz6uunsvr.us-east-1.rds.amazonaws.com';
-  $user= 'admin';
-  $pass='Group55Password!';
-  $db='bkldb';
-  $s3_url= 'https://group55-s3-public.s3.us-east-1.amazonaws.com';
-  
-  $conn= new mysqli($host, $user, $pass, $db) or die ("Failed to Connect to DB: %s\n". $conn -> error);
-  
-  $sql = "SELECT * FROM `Books`";
-  $query = $conn->query($sql);
-  while($result = $query -> fetch_array(MYSQLI_ASSOC))
-  {
-	  $file_name = $result['ISBN'] . ".jpg";
-	  $isbn = "ISBN: " . $result['ISBN'];
-	  $title = "Title: " . $result['Title'];
-	  $auth = "Author: " . $result['Author'];
-	  $publisher = "Publisher: " . $result['Publisher'];
-	  $pub_year = "Publishing Year: " . $result['Pub_year'];
-	  
-	  echo "<tr>";
-	  echo "<td><img src=\"$s3_url/$file_name\" alt=\"BookCover\" /></td>";
-	  echo "<td>
-				<div>$isbn</div>
-				<div>$title</div>
-				<div>$auth</div>
-				<div>$publisher</div>
-				<div>$pub_year</div>
+				<?php
+				$host='group55-rds-db.chynz6uunsvr.us-east-1.rds.amazonaws.com';
+				$user= 'admin';
+				$pass='Group55Password!';
+				$db='bkldb';
+				$s3_url= 'https://group55-s3-public.s3.us-east-1.amazonaws.com';
 				
-	        </td>";
-		
-	  echo "</tr>";
-	  
-  }
-  ?>
-			</table>
+				$conn= new mysqli($host, $user, $pass, $db) or die ("Failed to Connect to DB: %s\n". $conn -> error);
+				
+				$sql = "SELECT * FROM `Books`";
+				$query = $conn->query($sql);
+				while($result = $query -> fetch_array(MYSQLI_ASSOC))
+				{
+					$file_name = $result['ISBN'] . ".jpg";
+					$isbn = "ISBN: " . $result['ISBN'];
+					$title = "Title: " . $result['Title'];
+					$auth = "Author: " . $result['Author'];
+					$publisher = "Publisher: " . $result['Publisher'];
+					$pub_year = "Publishing Year: " . $result['Pub_year'];
+					
+					echo "<div class='book'>";
+						echo "<img src=\"$s3_url/$file_name\" alt=\"BookCover\" />";
+						echo "<div class='book-info'>";
+							echo "<h2>$title</h2>";
+							echo "<h3>by $auth</h3>";
+							echo "<p>Published in $pub_year by $publisher</p>";
+							echo "<p>ISBN: $isbn</p>";
+						echo "</div>";
+					echo "</div>";
+					
+				}
+				?>
 		</center>
 	</body>
 </html>
